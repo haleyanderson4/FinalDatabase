0d24d4cd8a02b060fcdd10f60efe55149a451883
sarahlasm <lasmansarah@gmail.com>
2018-12-14 01:12:23 -0800

    Fixed schema

--- Schema.sql
+++ Schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Company(companyId INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, companyName VARCHAR(50) not null, numEmployees INTEGER not null, yearlyRevenue FLOAT(20,2) not null, stockPrice FLOAT(20,2) not null);
 
-CREATE TABLE Job(jobId INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, jobTitle VARCHAR(50) not null, industry VARCHAR(25) not null, description VARCHAR(100) not null, companyId INTEGER not null, FOREIGN KEY companyId(companyId) REFERENCES Company(companyId), isInternship BOOL not null);
+CREATE TABLE Job(jobId INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, jobTitle VARCHAR(50) not null, industry VARCHAR(25) not null, description VARCHAR(100) not null, companyId INTEGER not null, FOREIGN KEY (companyId) REFERENCES Company(companyId), isInternship BOOL not null);
 
 CREATE TABLE Manager(managerId INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, companyId INTEGER not null, FOREIGN KEY (companyId) REFERENCES Company(companyId), name VARCHAR(50) not null, technicalExperience BOOL not null, yearsAtCompany INTEGER not null);
 
-CREATE TABLE RelatedJobs(jobId INTEGER PRIMARY KEY, FOREIGN KEY jobId(jobId) REFERENCES Job(jobId) ON DELETE CASCADE, related1 INTEGER, FOREIGN KEY (related1) REFERENCES Job(jobId) ON DELETE SET NULL, related2 INTEGER, FOREIGN KEY (related2) REFERENCES Job(jobId) ON DELETE SET NULL, related3 INTEGER, FOREIGN KEY (related3) REFERENCES Job(jobId) ON DELETE SET NULL, related4 INTEGER, FOREIGN KEY (related4) REFERENCES Job(jobId) ON DELETE SET NULL, 
+CREATE TABLE RelatedJobs(jobId INTEGER PRIMARY KEY, FOREIGN KEY (jobId) REFERENCES Job(jobId) ON DELETE CASCADE, related1 INTEGER, FOREIGN KEY (related1) REFERENCES Job(jobId) ON DELETE SET NULL, related2 INTEGER, FOREIGN KEY (related2) REFERENCES Job(jobId) ON DELETE SET NULL, related3 INTEGER, FOREIGN KEY (related3) REFERENCES Job(jobId) ON DELETE SET NULL, related4 INTEGER, FOREIGN KEY (related4) REFERENCES Job(jobId) ON DELETE SET NULL,
 related5 INTEGER, FOREIGN KEY (related5) REFERENCES Job(jobId) ON DELETE SET NULL);
 
 CREATE TABLE Competition(jobId INTEGER PRIMARY KEY AUTO_INCREMENT, FOREIGN KEY (jobId) REFERENCES Job(jobId) ON DELETE CASCADE, numOpenSpots INTEGER, numApplicants INTEGER);
